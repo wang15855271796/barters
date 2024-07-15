232a6788dc69da96e7d69acef173f464a026a003
wangtao12138 <1433953566@qq.com>
2024-07-15 17:27:21 +0800

    1

---
+++ app/src/main/java/com/barter/hyl/app/dialog/PermissionDialog.java
@@ -0,0 +1,44 @@
+package com.barter.procure.app.dialog;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.barter.procure.app.R;
+
+public abstract class PermissionDialog extends Dialog {
+    Context mContext;
+
+    public TextView tv_sure,tv_content;
+    public PermissionDialog(@NonNull Context context) {
+        super(context, R.style.promptDialog);
+        setContentView(R.layout.dialog_persmission);
+
+        mContext = context;
+
+        initView();
+        initAction();
+    }
+
+    private void initView() {
+        tv_sure= (TextView) findViewById(R.id.tv_sure);
+        tv_content = (TextView) findViewById(R.id.tv_content);
+
+    }
+
+
+    private void initAction() {
+        tv_sure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Confirm();
+            }
+        });
+    }
+
+    public abstract void Confirm();
+
+}
